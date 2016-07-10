e50b54d4cbf64cc947af494243147f790baf9280
Oleg verhovsky <verchol@gmail.com>
2016-07-10 03:32:31 +0300

    fix callback

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Hosts = require('./hosts');
 var debug = require('debug')('serviceDiscovery->Index');
+var _     = require('lodash');
 
 module.exports.watch  = function (fileToWatch, startFile, callback){
 debug('typeof callback ' + typeof callback);
@@ -8,7 +9,8 @@ if (arguments.length === 0)
 throw new Error('no callback provided');
 
 if (!callback)
-callback = arguments.slice(-1)[0];
+callback = _.last(arguments);
+
 //console.log(process.env.SkipWatcher);
 //console.log(process.env.NotBlockingWatcher);
 
@@ -19,9 +21,11 @@ if (process.env.NotBlockingWatcher)
     require('./bin/www')
 
 Hosts.watchFile(process.env.FILE_TO_WATCH || fileToWatch).then((data)=>{
+  debug('file exists')
   console.log(data);
+  callback.call(null, null ,data);
   process.env.NotBlockingWatcher || require(process.env.START_FILE || startFile);
-  callback(null ,data);
+
 }, callback);
 
 }
